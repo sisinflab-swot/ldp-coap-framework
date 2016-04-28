6078bb86aeca457d4c6c4bc47137c85cc22d6054
Giuseppe Loseto <giuseppe.loseto@gmail.com>
2016-04-28 18:00:20 +0200

    Added support for Test Suite server

--- ldp-coap/ldp-coap-core/src/main/java/it/poliba/sisinflab/coap/ldp/server/CoAPLDPTestSuiteServer.java
+++ ldp-coap/ldp-coap-core/src/main/java/it/poliba/sisinflab/coap/ldp/server/CoAPLDPTestSuiteServer.java
@@ -1,43 +1,56 @@
 package it.poliba.sisinflab.coap.ldp.server;
 
+import org.eclipse.californium.core.network.config.NetworkConfig;
+
 import it.poliba.sisinflab.coap.ldp.LDP;
 import it.poliba.sisinflab.coap.ldp.resources.CoAPLDPDirectContainer;
 import it.poliba.sisinflab.coap.ldp.resources.CoAPLDPIndirectContainer;
 import it.poliba.sisinflab.rdf.vocabulary.SSN_XG;
 
-public class CoAPLDPTestSuiteServer {
+public class CoAPLDPTestSuiteServer extends CoAPLDPServer {
 	
 	final static String BASE_URI = "coap://192.168.2.16:5683";
-	static CoAPLDPServer server;
     
     /*
      * Application entry point.
      */
     public static void main(String[] args) {
         
-        // create server
-		server = new CoAPLDPServer(BASE_URI);
-		
-		init();
-		
-		server.start();
+        // create Test Suite server
+    	CoAPLDPTestSuiteServer server = new CoAPLDPTestSuiteServer(BASE_URI);
+    	server.start();
+    }
+    
+    public CoAPLDPTestSuiteServer(){
+    	super(BASE_URI);		
+		init();		
+    }
+    
+    public CoAPLDPTestSuiteServer(String URI){
+    	super(URI);		
+		init();		
+    }
+    
+    public CoAPLDPTestSuiteServer(String URI, NetworkConfig config, int port){
+    	super(URI, config, port);		
+		init();		
     }
     
     /*
      * Constructor for a new CoAP LDP server. Here, the resources
      * of the server are initialized.
      */
-    public static void init() {
+    private void init() {
     	
     	/*** Handle read-only properties ***/
-    	server.setReadOnlyProperty("http://purl.org/dc/terms/created");
-    	server.setConstrainedByURI("http://sisinflab.poliba.it/swottools/ldp-coap/ldp-report.html");
+    	setReadOnlyProperty("http://purl.org/dc/terms/created");
+    	setConstrainedByURI("http://sisinflab.poliba.it/swottools/ldp-coap/ldp-report.html");
     	
     	/*** Handle not persisted properties ***/
-    	server.setNotPersistedProperty("http://example.com/ns#comment");
+    	setNotPersistedProperty("http://example.com/ns#comment");
         
     	/*** Add LDP-DirectContainer ***/
-    	CoAPLDPDirectContainer dc = server.createDirectContainer("ldp", "resources", LDP.CLASS_RDFSOURCE, LDP.PROP_MEMBER, null);
+    	CoAPLDPDirectContainer dc = createDirectContainer("ldp", "resources", LDP.CLASS_RDFSOURCE, LDP.PROP_MEMBER, null);
     	dc.setRDFTitle("Product description of LDP Demo product which is also an LDP-DC");  
       	
       	/*** Add LDP-IndirectContainer in LDP-DirectContainer ***/
