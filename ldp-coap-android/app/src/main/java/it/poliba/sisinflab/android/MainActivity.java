694f7206dafb25ba09533dc73de82ef066f894de
Giuseppe Loseto <giuseppe.loseto@gmail.com>
2016-07-12 16:13:25 +0200

    Update to RDF4J

--- ldp-coap-android/app/src/main/java/it/poliba/sisinflab/android/MainActivity.java
+++ ldp-coap-android/app/src/main/java/it/poliba/sisinflab/android/MainActivity.java
@@ -5,6 +5,7 @@ import android.hardware.Sensor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,7 +15,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 import org.eclipse.californium.core.CoapResource;
